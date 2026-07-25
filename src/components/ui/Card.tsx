bb69d2fcfdc27e451f46e623df5d91acc95e75b8
Cris <cris@soffi.ai>
2026-07-25 16:22:43 +0000

    AI: Edit src/components/ui/Card.tsx

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/cn'
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const cardVariants = cva('flex flex-col bg-(--color-surface)', {
   variants: {
     variant: {

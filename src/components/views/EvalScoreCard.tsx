71878018b49dd12c9353cbb017bf6f3cfa8285bb
Cris <cris@soffi.ai>
2026-08-06 20:44:48 +0000

    AI: Edit src/components/views/EvalScoreCard.tsx

--- src/components/views/EvalScoreCard.tsx
+++ src/components/views/EvalScoreCard.tsx
@@ -7,6 +7,7 @@ import { Heading } from '@/components/ui/Heading'
 import { ProgressBar } from '@/components/ui/ProgressBar'
 import { Badge } from '@/components/ui/Badge'
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const evalScoreCardVariants = cva('flex flex-col gap-3', {
   variants: {
     severity: {
